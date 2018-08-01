93ca30f5a9ad11fae82513d8d24496219c05f193
manna91 <anna.mate4@gmail.com>
2018-08-01 17:16:44 +0200

    Update Chuck_Norris.cs

--- Chuck_Norris.cs
+++ Chuck_Norris.cs
@@ -5,10 +5,7 @@ using System.Text;
 using System.Collections;
 using System.Collections.Generic;
 
-/**
- * Auto-generated code below aims at helping you parse
- * the standard input according to the problem statement.
- **/
+
 class Solution
 {
     static void Main(string[] args)
@@ -18,40 +15,40 @@ class Solution
 
         byte[] asciiMessage = Encoding.ASCII.GetBytes(MESSAGE);
            
-        Array.Reverse(asciiMessage);    //byte tömb megfordítása
+        Array.Reverse(asciiMessage);    
         
        
-        BitArray bitMessage = new BitArray(asciiMessage);   //byte tömb átkonvertálása bináris tömbbé (lsb first - először a legkisebb helyiértékű bitet írja ki)
+        BitArray bitMessage = new BitArray(asciiMessage);   
         
-        string outputMSG = "";  //kimeneti üzenet
-        int zeroDB = 0;     //0-k számolása
-        int oneDB = 0;      //1-ek számolása
-        int state = 0;  //állapotváltozó
+        string outputMSG = "";  
+        int zeroDB = 0;     
+        int oneDB = 0;      
+        int state = 0; 
         
-        for(int i=(bitMessage.Length-1);i>=0;i--)   //hátulról megyünk végig a tömb elemein (msb first), így helyes lesz a byte sorrend és a bit sorrend is
+        for(int i=(bitMessage.Length-1);i>=0;i--)  
         {
-            if(i % 8 != 7)      //az egyes byte-oknál az msb-t nem vesszük figyelembe, mivel az ASCII karakter 7 bites
+            if(i % 8 != 7)     
             {
                 
                 switch(state)
                 {
-                    case 0:         // előző bit "0" volt (vagy most dolgozzuk fel az első bitet)
+                    case 0:         
                         
-                        if(bitMessage[i]==true)     //a mostani bit "1"
+                        if(bitMessage[i]==true)     
                         {
-                            state = 1;      //beállítom az állapotot 1-re
+                            state = 1;      
                             oneDB++;
-                            if(zeroDB != 0)     //ha már olvastunk be "0"-t
+                            if(zeroDB != 0)     
                             {
                                 outputMSG += "00 ";
-                                for(int z=0;z<zeroDB;z++)   //zeroDB darab 0-t hozzáadok a kimeneti sztringhez
+                                for(int z=0;z<zeroDB;z++)   
                                 {
                                     outputMSG += "0";
                                 }
                                 outputMSG += " ";
                             }
                             zeroDB = 0;
-                            if(i==0)				// utolsó "1"-es kiírása
+                            if(i==0)				
                             {
                                 outputMSG += "0 0";
                             }
@@ -61,7 +58,7 @@ class Solution
                         {
                             state = 0;
                             zeroDB++;
-                            if(i==0)			// maradék "0"-k kiírása
+                            if(i==0)			
                             {
                                 outputMSG += "00 ";
                                 for(int z=0;z<zeroDB;z++)
@@ -73,13 +70,13 @@ class Solution
                         
                         break;
                         
-                    case 1:         // előző bit "1" volt
+                    case 1:        
                         
                         if(bitMessage[i]==true)
                         {
                             state = 1;
                             oneDB++;
-                            if(i==0)				// maradék "1"-esek kiírása
+                            if(i==0)				
                             {
                                 outputMSG += "0 ";
                                 for(int o=0;o<oneDB;o++)
@@ -88,7 +85,7 @@ class Solution
                                 }
                             }
                         }
-                        else        //előző bit "1", és most "0"-t kaptunk
+                        else        
                         {
                             state = 0;
                             zeroDB++;
@@ -97,14 +94,14 @@ class Solution
                                 outputMSG += "0 ";
                                 for(int o=0;o<oneDB;o++)
                                 {
-                                    outputMSG += "0";   //ha volt "1"-es, akkor azokat kiiratjuk
+                                    outputMSG += "0";   
                                 }
                                 outputMSG += " ";
                             }
                             oneDB = 0;
                             if(i==0)			
                             {
-                                outputMSG += "00 0";    //utolsó "0" kiírása
+                                outputMSG += "00 0";   
                             }
                         }
                         break;
